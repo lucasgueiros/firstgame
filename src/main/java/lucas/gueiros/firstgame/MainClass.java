effeafbad12eba752b7b142e03e826a542a0ee6d
Lucas Gueiros <lucasdantasgueiros@gmail.com>
2020-08-21 13:24:25 -0300

    Logging working

--- src/main/java/lucas/gueiros/firstgame/MainClass.java
+++ src/main/java/lucas/gueiros/firstgame/MainClass.java
@@ -2,9 +2,14 @@ package lucas.gueiros.firstgame;
 
 import java.awt.EventQueue;
 import lucas.gueiros.firstgame.application.App;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class MainClass {
+  final static Logger logger = LogManager.getLogger(MainClass.class);
+
   public static void main (String [] args) {
+    logger.atInfo().log("Iniciando aplicação");
     EventQueue.invokeLater(() -> {
             App ex = new App();
             ex.setVisible(true);
